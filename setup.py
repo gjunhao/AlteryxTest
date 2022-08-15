0209532d0c6fc9eabf051054e1e0937d1258352c
Jun Hao Goh <junhao.goh@billigence.com>
2022-08-15 15:06:16 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AlteryxTest',
+    version='',
+    packages=['main'],
+    url='',
+    license='',
+    author='Jun Hao Goh',
+    author_email='',
+    description=''
+)
